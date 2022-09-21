b2d45923a0abb9e8b740096c3d2d8dbd76d97869
AleksandrApoltsev <docapoltsev@yandex.ru>
2022-09-21 19:29:52 +0400

    automation practice form

--- src/test/java/com/demoqa/AutomationPracticeForm.java
+++ src/test/java/com/demoqa/AutomationPracticeForm.java
@@ -4,6 +4,12 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.awt.desktop.OpenFilesEvent;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class AutomationPracticeForm {
 
     @BeforeAll
@@ -15,7 +21,35 @@ public class AutomationPracticeForm {
     }
 
     @Test
-    void
+    void formTest(){
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Aleksandr");
+        $("#lastName").setValue("Apoltsev");
+        $("#userEmail").setValue("apoltsev@mail.ru");
+        $("#userNumber").setValue("9876543210");
+        $("#currentAddress").setValue("City Street 123456");
+        $("#subjectsInput").setValue("English").pressEnter();
+
+        $("#uploadPicture").uploadFromClasspath("1.jpg");
+
+        $(byText("Male")).click();
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1992");
+        $(".react-datepicker__day--008").click();
+
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        //$("#submit").click();
+
+
+    }
 
 
 
